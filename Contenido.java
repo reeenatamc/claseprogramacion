fc9ab77670965a6e27757a1d3ca2bb3f3ae13eb2
reeenatamc <renataxdalej@gmail.com>
2022-10-25 10:33:14 -0500

    Trabajo Git

---
+++ Contenido.java
@@ -0,0 +1,25 @@
+public class Contenido {
+    public static void main(String[] args) {
+        System.out.println("Un programa es un conjunto de instrucciones que permiten ordenar el proceso para resolver un problema");
+        System.out.println("El algoritmo describe la solución de problemas a través de un computador");
+        System.out.println("El algoritmo tiene tres partes; entradas, procesos y salidas");
+        System.out.println("La lógica de programación es la organización coherente de instrucciones para la creación de programas");
+        System.out.println("Primero se crea el algoritmo y luego el programa");
+        System.out.println("El sistema binario es un sistema de numeración natural de las computadoras, donde solo hay 0 y 1");
+        System.out.println("El Bit es el elemento de dato más pequeño en una computadora");
+        System.out.println("El Byte es el conjunto de 8 bits y representa la unidad mínima de almacenamiento de un computador");
+        System.out.println("Algunos de los múltiplos del byte son el kilobyte, megabyte, gigabyte, tetabyte, etc");
+        System.out.println("Hay una jerarquía de datos que permite representar la información digitalmente");
+        System.out.println("Los tipos de datos básicos son los enteros, reales, cadena de texto, caracter, booleano.");
+        System.out.println("La programación da vida al hardware");
+        System.out.println("La programación es importante porque comunica instrucciones a los dispositivos");
+        System.out.println("Un software es un conjunto de instrucciones que permiten ordenar a la computadora el proceso para resolver un problema");
+        System.out.println("Existen soffware de sistema y software de aplicación");
+        System.out.println("Un  programa se escribe en un lenguaje de programación");
+        System.out.println("Se usa java, c++, phyton, etc");
+        System.out.println("Las instrucciones que expresan un algoritmo se llaman programación");
+        System.out.println("Un entorno de programación es un programa que facilita al programador el desarrollo de programas");
+        System.out.println("Un IDE es un entorno de desarrollo integrado");
+
+    }
+}
