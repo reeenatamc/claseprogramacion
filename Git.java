36ed21cf0207f6cdb4001753433f533af5c00055
reeenatamc <renataxdalej@gmail.com>
2022-10-25 11:26:53 -0500

    clasesprogramacion

---
+++ Git.java
@@ -0,0 +1,10 @@
+public class Git {
+    public static void main(String[] args) {
+        System.out.println("Los comandos usados para la inicialización del proyecto a  git hasta la colocación de los archivos en github son:");
+        System.out.println("git init: Inicia el repositorio local del computador.");
+        System.out.println("git add .: Seguimiento de los cambios realizados, indica a Git que se requiere incluir actualizaciones en un archivo en específico en la próxima confirmación.");
+        System.out.println("git commit -m: Prepara los cambios añadidos para push them (empujarlos) a GitHub ");
+        System.out.println("git remote add origin: Conecta la carpeta local al repositorio de GitHub con el enlace que se proporcione");
+        System.out.println("git push -u origin master: En palabras simples, este comando sube el proyecto a GitHub ");
+    }
+}
